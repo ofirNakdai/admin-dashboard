2291ee8871deb73022e9490ee7b9ef7178bad3de
Ofir Nakdai <ofirnakdai@gmail.com>
2024-09-01 15:28:30 +0300

    setting FAQ Accordion to be default expanded

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -27,7 +27,7 @@ const FAQ = () => {
     <Box m="20px">
       <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
       {questions.map((question) => (
-        <Accordion>
+        <Accordion defaultExpanded>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
             <Typography color={colors.greenAccent[500]} variant="h5">
               {question.summary}
