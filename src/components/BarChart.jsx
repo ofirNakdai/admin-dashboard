ba4d8f3669f7acb5c2678d7645c2da1d18d5fb73
Ofir Nakdai <ofirnakdai@gmail.com>
2024-09-01 16:48:09 +0300

    creating PieChart with nivo

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -3,7 +3,7 @@ import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockData";
 
-const BarChart = ({isDashboard = false}) => {
+const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -19,7 +19,7 @@ const BarChart = ({isDashboard = false}) => {
           },
           legend: {
             text: {
-              fill: colors.grey[100],
+              fill: colors.greenAccent[500],
             },
           },
           ticks: {
